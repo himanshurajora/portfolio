1071a6d54e3558efc4a252d4c1b69edf147e6c4a
Himanshu Jangid <iamahalofan@gmail.com>
2022-09-26 22:36:00 +0530

    dark theme

--- theme.config.js
+++ theme.config.js
@@ -3,6 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
+      <br />
       <time>{YEAR}</time> © Himanshu Jangid.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
